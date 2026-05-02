b73d5b6317e41641fe4f5a0666d3600ad302276e
Arthur <arthurwheildon0@gmail.com>
2026-05-02 10:44:22 +0100

    Initial Commit

---
+++ Willow/WillowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WillowApp.swift
+//  Willow
+//
+//  Created by Arthur  on 02/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct WillowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WillowTests/WillowTests.swift
@@ -0,0 +1,19 @@
+//
+//  WillowTests.swift
+//  WillowTests
+//
+//  Created by Arthur  on 02/05/2026.
+//
+
+import Testing
+@testable import Willow
+
+struct WillowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
